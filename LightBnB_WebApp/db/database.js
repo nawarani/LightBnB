736728a054628955d9a6d82384de18a9ea180bea
Anika Nawar <nawarani47@gmail.com>
2025-03-17 11:57:20 -0400

    price per night separated min from max

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -125,9 +125,12 @@ const getAllProperties = (options, limit = 10) => {
     queryString += `AND owner_id = $${queryParams.length} `;
   }
 
-  if (options.minimum_price_per_night && options.maximum_price_per_night) {
+  if (options.minimum_price_per_night) {
     queryParams.push(`${options.minimum_price_per_night * 100}`);
     queryString += `AND cost_per_night >= $${queryParams.length} `;
+  }
+
+  if (options.maximum_price_per_night) {
     queryParams.push(`${options.maximum_price_per_night * 100}`);
     queryString += `AND cost_per_night <= $${queryParams.length} `;
   }
